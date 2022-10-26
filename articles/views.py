c6c9b083de0cf332a897539d33bcadffe6ebc96d
didjsrne <hyunjae0723@gmail.com>
2022-10-26 10:30:16 +0900

    class, 함수형태 정리

--- articles/views.py
+++ articles/views.py
@@ -9,7 +9,7 @@ from articles.serializers import ArticleSerializer
         
     
 
-class index(APIView):
+class ArticleList(APIView):
     def get(self, request, format=None):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
@@ -22,3 +22,20 @@ class index(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+'''
+@api_view(['GET', 'POST'])
+def index(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+'''
