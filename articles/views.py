8736c2c47742a2facfa5d7a697475ef568b78943
야언 <112169271+didjsrne@users.noreply.github.com>
2022-10-26 10:33:13 +0900

    함수형 적용시 필요한 api_view import 추가

--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from articles.models import Article
 from articles.serializers import ArticleSerializer
+# from rest_framework.decorators import api_view
+
 
 # Create your views here.
 
