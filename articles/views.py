21e5d6c48873df6c05162e51869af68122ffebdb
didjsrne <hyunjae0723@gmail.com>
2022-10-26 09:42:32 +0900

    init project

---
+++ articles/views.py
@@ -0,0 +1,20 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from articles.models import Article
+from articles.serializers import ArticleSerializer
+# Create your views here.
+
+class ArticleList(APIView):
+    def get(self, request, format=None):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
